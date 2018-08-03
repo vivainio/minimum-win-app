004cb63d283764fd8ab9ddcbd98f7eca1bc52452
Ville M. Vainio <ville.vainio@basware.com>
2018-08-03 22:22:51 +0300

    exitprocess

--- main.c
+++ main.c
@@ -5,7 +5,8 @@ mainCRTStartup(void)
 {
     char msg[] = "Hello, world!\n";
     HANDLE stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
     WriteFile(stdout, msg, sizeof(msg), (DWORD[]){0}, NULL);
-    return 0;
+    ExitProcess(0);
 }
 
