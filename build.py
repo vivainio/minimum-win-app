f7597efdc64e65598e55583cc3034e67d69011cc
Ville M. Vainio <ville.vainio@basware.com>
2018-08-03 22:14:56 +0300

    add files

---
+++ build.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("gcc -std=c99 -Wall -Wextra \
+    -nostdlib -ffreestanding -mconsole -Os \
+    -fno-stack-check -fno-stack-protector -mno-stack-arg-probe \
+    -o main.exe main.c \
+    -lkernel32")
